508b7be6d09bae8e13c5691405a0bcd4d8154ae2
Ahmed hamdy <es.ahmedhamdy.19@gmail.com>
2019-05-14 03:00:22 +0200

    Final Arduino Code

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,54 +1,70 @@
-#define GRIPPER 11 
-#define TOP 10 
-#define BOTTOM 6 
-#define BASE 5 
+#define GRIPPER 11
+#define TOP 10
+#define BOTTOM 6
+#define BASE 5
 
-#include <Servo.h> 
+#include <Servo.h>
 
-Servo gripper ; 
-Servo top ; 
-Servo bottom ; 
-Servo base ; 
+Servo gripper ;
+Servo top ;
+Servo bottom ;
+Servo base ;
 
-unsigned char motorToMove = 0 ; 
-unsigned char angle = 0 ; 
+unsigned char motorToMove = 0 ;
+unsigned char angle = 0 ;
 
-void setup() 
+void setup()
 {
-  Serial.begin(9600) ; 
+  Serial.begin(9600) ;
   gripper.attach(GRIPPER) ;
-  top.attach(TOP) ; 
-  bottom.attach(BOTTOM) ; 
-  base.attach(BASE) ;    
-}
+  gripper.write(50);
 
-void loop()
-{
-  String command = Serial.readString() ; 
-  control( command );     
-}
+  top.attach(TOP) ;
+  top.write(80);
+
+  bottom.attach(BOTTOM) ;
+  bottom.write(90);
+
+  base.attach(BASE) ;
+  base.write(90);
+
+  Serial.setTimeout(50);
+ }
 
 void control(String controlCommand )
 {
-    unsigned char index = controlCommand.indexOf('/') ; 
-    motorToMove = controlCommand.substring( 0 , index ).toInt() ; 
-    angle = controlCommand.substring(index + 1 ).toInt() ;
+  unsigned char index = controlCommand.indexOf('/') ;
+  motorToMove = controlCommand.substring( 0 , index ).toInt() ;
+  angle = controlCommand.substring(index + 1 ).toInt() ;
 
-    switch( motorToMove ) 
-    {
-      case 0 : 
-        break ; 
-      case 4 :
-        gripper.write( angle ) ; 
-        break ;  
-      case 3 : 
-        top.write( angle ) ; 
-        break ; 
-      case 2 : 
-        bottom.write( angle ) ; 
-        break ; 
-      case 1 : 
-        base.write( angle ) ; 
-        break ; 
+  switch( motorToMove )
+  {
+    case 0 :
+      break ;
+
+    case 4 :
+      gripper.write( angle ) ;
+      break ;
+
+    case 3 : 
+      angle=angle<5?5:angle;
+      top.write(angle) ; 
+      break  ; 
+       
+    case 2 :
+      angle=angle<5?5:angle;
+      bottom.write(angle) ; 
+      break ; 
+      
+    case 1 :
+      angle=angle<5?5:angle;
+      base.write( angle ) ;
+      break ;
     }
+  }
+
+void loop()
+{
+  String command = Serial.readString() ;
+  control( command );
 }
